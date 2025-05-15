93454e3092a1d5733cb1a48b3e02d79102983253
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-05-16 09:56:20 +1000

    Created base app
    Moved all code from CodeSandbox into VS Code and deploying to GitHub

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import WeatherSearch from "./WeatherSearch";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <WeatherSearch />
+    </div>
   </React.StrictMode>
 );
 
