60e14698ae6d78c692e34028f7b77d106b531e6c
smilligan-sm <sarahlouisemilligan.sm@gmail.com>
2025-05-16 13:52:37 +1000

    Fixed link issue from Netlify and footer spacing
    Added rel="noreferrer" to all links, added line breaks before footer.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <SearchEngine />
+      <br></br>
+      <br></br>
         <footer>
-      Built by <a href="https://github.com/smilligan-sm" target="_blank">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/React-weather-app" target="_blank">Github</a
-      > and hosted on <a href="https://sm-react-weather-app.netlify.app/" target="_blank">Netlify</a>
+      Built by <a href="https://github.com/smilligan-sm" target="_blank" rel="noreferrer">Sarah Milligan</a>, is open-sourced on <a href="https://github.com/smilligan-sm/React-weather-app" target="_blank" rel="noreferrer">Github</a
+      > and hosted on <a href="https://sm-react-weather-app.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
     </footer>
     </div>
   );
